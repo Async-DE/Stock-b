62b3a2620f4dd8c6864502ba67b13701f0c7f347
JIDE-73 <jimenezleonardo875@gmail.com>
2026-02-04 16:03:14 -0800

    Configuracion basica del index

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// ejecutador de la aplicacion
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import ubicacionesRoutes from "./app/routes/Ubicaciones/ubicaciones.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "https://0.0.0.0/0",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  }),
+);
+
+app.use("/stock", ubicacionesRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
