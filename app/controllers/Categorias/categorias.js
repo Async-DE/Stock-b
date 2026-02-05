12d7ff039138bd76506699fe2f20da6cb20994c5
Choco-Leo <zepelini2307@hotmail.com>
2026-02-05 00:07:36 -0600

    Creacion, yActualizacion Productos, - Correccion de Validacion

--- app/controllers/Categorias/categorias.js
+++ app/controllers/Categorias/categorias.js
@@ -5,10 +5,7 @@ const createCategoria = async (req, res) => {
   const { nombre } = req.body;
 
   // Validar los datos de entrada
-  await check("nombre", "El nombre es obligatorio").notEmpty().run(req);
-
-  //Validar tipos de datos
-  await check("nombre", "El nombre debe ser una cadena de texto").isString().run(req);
+  await check("nombre").notEmpty().isString().withMessage("El nombre es obligatorio y debe ser una cadena de texto").run(req);
 
   const errors = validationResult(req);
 
@@ -25,7 +22,6 @@ const createCategoria = async (req, res) => {
 
     res.status(201).json("Categoría creada con éxito");
   } catch (error) {
-    console.error("Error al crear la categoría:", error);
     res.status(500).json({ error: "Error al crear la categoría" });
   }
 };
@@ -33,6 +29,16 @@ const createCategoria = async (req, res) => {
 const updateCategoria = async (req, res) => {
   const { id } = req.params;
   const { nombre } = req.body;
+
+  // Validar los datos de entrada
+  await check("nombre").notEmpty().isString().withMessage("El nombre es obligatorio y debe ser una cadena de texto").run(req);
+
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
   try {
     const categoriaExistente = await prisma.categorias.findUnique({
       where: { id: parseInt(id) },
@@ -46,7 +52,6 @@ const updateCategoria = async (req, res) => {
     });
     res.status(200).json("Categoría actualizada con éxito");
   } catch (error) {
-    console.error("Error al actualizar la categoría:", error);
     res.status(500).json({ error: "Error al actualizar la categoría" });
   }
 };
