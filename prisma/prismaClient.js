cb0ad4bb58a5bae01c829487c2eea93224f9d888
Choco-Leo <zepelini2307@hotmail.com>
2026-02-05 20:27:48 -0600

    Correccion Funcionamiento

--- prisma/prismaClient.js
+++ prisma/prismaClient.js
@@ -1,14 +1,3 @@
-import { createRequire } from "module";
-import "dotenv/config";
-import pg from "pg";
-import { PrismaPg } from "@prisma/adapter-pg";
-
-const require = createRequire(import.meta.url);
-const { PrismaClient } = require(".prisma/client/index.js");
-
-const { Pool } = pg;
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-const adapter = new PrismaPg(pool);
-const prisma = new PrismaClient({ adapter });
-
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 export default prisma;
