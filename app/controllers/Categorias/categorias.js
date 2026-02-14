1189410c58c7c4aa913cbcf761ed6bfaee251130
JIDE-73 <jimenezleonardo875@gmail.com>
2026-02-13 17:41:44 -0800

    respuesta mas limpia

--- app/controllers/Categorias/categorias.js
+++ app/controllers/Categorias/categorias.js
@@ -89,7 +89,8 @@ const getCategorias = async (req, res) => {
         subcategorias: true
       }
     });
-    res.status(200).json(categorias);
+
+    res.status(200).json({ message: "Categorías obtenidas con éxito", categorias });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Error al obtener categorías" });
